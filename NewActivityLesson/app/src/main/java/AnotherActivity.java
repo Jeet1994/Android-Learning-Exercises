f60caef200fd79ccf433d1fed195fecc2825f592
Jeet1994 <pragyaditya.das94@gmail.com>
2016-04-19 00:54:45 +0530

    Go from one activity to another. Tested on Samsung J2

---
+++ NewActivityLesson/app/src/main/java/AnotherActivity.java
@@ -0,0 +1,19 @@
+/**
+ * Created by user on 15-04-2016.
+ */
+
+package com.jeet.user.newactivitylesson;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
+
